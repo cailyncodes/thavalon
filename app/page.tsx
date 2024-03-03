e55580e9cf1eda8e999c480e5f2933defc079a91
Caleb Noble <caleb.noble@duolingo.com>
2024-03-03 11:01:21 -0500

    polish

--- app/page.tsx
+++ app/page.tsx
@@ -39,7 +39,7 @@ export default function Home() {
             className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
             type="submit"
           >
-            Join
+            Join a Game
           </button>
         </form>
         <form className="w-full max-w-lg flex justify-center items-center" action={createNewGame}>
