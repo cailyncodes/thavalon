326e4d95d9de2def40a1c5eaec49eff6a1b38b10
Cailyn Hansen (she/her) <cailyn.e.hansen+github@gmail.com>
2024-03-11 23:48:02 -0400

    Remove player menu to streamline information (#8)
    * Remove player menu to streamline information
    
    * Remove deprecated
    
    * Update GameView.tsx

---
+++ app/game/[gameId]/view/GameView.tsx
@@ -0,0 +1,78 @@
+"use client"
+
+import React from "react"
+import { Game } from "../../index"
+import { RemakeGame } from "./RemakeGame"
+
+interface Props {
+  game: Game
+  gameCode: string
+}
+
+export const GameView = ({ game, gameCode }: Props) => {
+  const [username, setUsername] = React.useState<string>();
+  const [shouldShowDoNotOpen, setShouldShowDoNotOpen] = React.useState<boolean>(false);
+
+  React.useEffect(() => {
+    const name = window.localStorage.getItem("username");
+    if (name) {
+      setUsername(name);
+    }
+  }, []);
+
+  if (!username) {
+    return null
+  }
+
+  return (
+    <>
+      {game &&
+        <div className="max-w-5xl w-full font-mono text-sm flex flex-col">
+          <div className="max-w-lg w-full h-full flex flex-col self-center items-center justify-center mb-6 text-lg">
+            <h2>Game Code: {gameCode}</h2>
+          </div>
+          <div className="max-w-lg w-full h-full flex flex-col self-center items-center justify-center">
+            <div className="flex flex-row justify-center items-center w-full mb-6">
+              <p className='text-md font-bold'>{game.start}</p>
+            </div>
+            <div className="flex flex-col justify-center items-center w-full mb-6">
+              <div className="flex flex-row justify-center items-center w-full mb-4">
+                <div className="max-w-3xl w-full font-mono text-sm flex flex-col">
+                  <pre className="whitespace-pre-line">
+                    {game[username]}
+                  </pre>
+                </div>
+              </div>
+            </div>
+          </div>
+          {shouldShowDoNotOpen ? (
+            <div className="max-w-lg w-full h-full flex flex-col self-center items-center justify-center mb-6">
+              <div className="flex flex-row justify-center items-center w-full mb-4">
+                <div className="max-w-3xl w-full font-mono text-sm flex flex-col">
+                  <pre className="whitespace-pre-line">
+                    {game["doNotOpen"]}
+                  </pre>
+                </div>
+              </div>
+            </div>
+          ) : (
+            <div className="max-w-lg w-full h-full flex flex-col self-center items-center justify-center mb-6">
+              <button
+                className="bg-red-500 hover:bg-red-700 text-white font-bold py-2 px-4 rounded w-2/5"
+                onClick={() => {
+                  if (window.confirm("Are you sure? Cancel to close.")) {
+                    setShouldShowDoNotOpen(true)
+                  }
+                }}
+              >
+                Show Do Not Open
+              </button>
+            </div>
+          )
+          }
+          <RemakeGame />
+        </div>
+      }
+    </>
+  )
+}
--- app/game/[gameId]/view/page.tsx
+++ app/game/[gameId]/view/page.tsx
@@ -1,6 +1,6 @@
 import { getGame } from '../../index'
 import React from 'react'
-import { RemakeGame } from './RemakeGame'
+import { GameView } from './GameView'
 
 export default async function GameHome({
   params,
@@ -14,29 +14,7 @@ export default async function GameHome({
 
   return (
     <main className="min-h-screen h-full flex flex-col items-center justify-center p-24">
-      {game &&
-        <div className="max-w-5xl w-full font-mono text-sm flex flex-col">
-          <div className="mb-12">
-            <h1 className="text-lg">Players</h1>
-          </div>
-          <div className="max-w-lg w-full h-full flex flex-col self-center items-center justify-center mb-6 text-lg">
-            <h2>Game Code: {gameCode}</h2>
-          </div>
-          <div className="max-w-lg w-full h-full flex flex-col self-center items-center justify-center">
-            <div className="flex flex-row justify-center items-center w-full mb-6">
-              <p className='text-md font-bold'>{game.start}</p>
-            </div>
-            <div className="flex flex-col justify-center items-center w-full mb-6 lg:mb-12">
-              {game.players.map((player, index) => (
-                <div key={index} className="flex flex-row justify-center items-center w-full mb-4">
-                  <a href={`/game/${params.gameId}/view/${player}`} className="underline underline-offset-2">{player}</a>
-                </div>
-              ))}
-            </div>
-          </div>
-          <RemakeGame />
-        </div>
-      }
+      <GameView game={game} gameCode={gameCode} />
     </main>
   )
 }
