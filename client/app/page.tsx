1a89ce7ed9584e326087412e181e8c8c6111f49a
Cailyn Hansen <cailyn@duolingo.com>
2024-03-24 01:37:06 -0400

    Update Thavalon Name

--- client/app/page.tsx
+++ client/app/page.tsx
@@ -6,7 +6,7 @@ const Home = () => {
     <main className="flex min-h-screen flex-col items-center justify-center p-24">
       <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex mb-12">
         <div className="w-full flex flex-col items-center justify-between">
-          <h1 className="text-4xl font-bold">THAvalon</h1>
+          <h1 className="text-4xl font-bold">THAvalon for Fun</h1>
         </div>
       </div>
       <div className="w-full flex flex-col items-center justify-between">
