91fa18158884c10f8b329e11f8efbf923e196f14
Cailyn Hansen (she/her) <cailyn.e.hansen+github@gmail.com>
2024-04-20 00:46:13 -0400

    Make 9 players games with 4 evil

--- api/main.py
+++ api/main.py
@@ -166,7 +166,7 @@ def get_player_info(player_names, variant = "thavalon"):
     print("Here is the number of players: ", num_players)
     if num_players < 7:
         num_evil = 2
-    elif num_players < 10:
+    elif num_players < 9:
         num_evil = 3
     else:
         num_evil = 4
