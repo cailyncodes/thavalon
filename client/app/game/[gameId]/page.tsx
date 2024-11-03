5b2a75b76bf2025465943c210792b070ff3d1799
Cailyn Hansen (she/her) <cailyn.e.hansen+github@gmail.com>
2024-11-03 08:55:09 -0500

    Fix staging and main environments (#17)
    * Fix app start up and domain name issues

--- client/app/game/[gameId]/page.tsx
+++ client/app/game/[gameId]/page.tsx
@@ -1,6 +1,32 @@
 import React from "react";
 import { GameManager } from "./GameManager";
 
+
+type CommunicationChannel = "ws" | "http"
+
+function getDomain(env?: string) {
+  env = env || 'development'
+
+  if (env.startsWith("thavalon-")) {
+    return `api-${env}.up.railway.app`
+  }
+  switch (env) {
+    case 'development':
+      return 'localhost:6464'
+    case 'next':
+      return 'next-api.thavalon.quest'
+    case 'production':
+      return "api.thavalon.quest"
+    default:
+      throw new Error('Unknown environment')
+  }
+}
+
+function getUrl(env: string | undefined, channel: CommunicationChannel) {
+  const origin = getDomain(env);
+  return origin?.includes('localhost') ? `${channel}://${origin}` : `${channel}s://${origin}`
+}
+
 export default function GameHome({
   params,
   searchParams,
@@ -8,6 +34,9 @@ export default function GameHome({
   params: { gameId: string };
   searchParams: { [key: string]: string | string[] | undefined }
 }) {
+  const wsUrl = getUrl(process.env.RAILWAY_ENVIRONMENT_NAME, "ws")
+  const httpUrl = getUrl(process.env.RAILWAY_ENVIRONMENT_NAME, "http")
+
   const gameCodeParam = searchParams['gameCode']
 
   let gameCode: string | undefined = undefined
@@ -21,6 +50,8 @@ export default function GameHome({
     <GameManager
       gameId={params.gameId}
       gameCode={gameCode}
+      wsUrl={wsUrl}
+      httpUrl={httpUrl}
     />
   )
 }
