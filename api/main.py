61869647977658219a61257e37dee46f62772259
Cailyn Hansen <cailyn@duolingo.com>
2024-11-02 23:31:18 -0400

    Try fix to test

--- api/main.py
+++ api/main.py
@@ -682,4 +682,5 @@ def test():
 
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=os.getenv("PORT", default=5000))
+    test()
+    # app.run(host="0.0.0.0", port=os.getenv("PORT", default=5000))
